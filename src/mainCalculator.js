b6f81a8c2e694b213149aa536fff036dd9b086e5
calexandredev <calexandredev@gmail.com>
2020-06-09 09:28:27 -0300

    Start run tests

--- src/mainCalculator.js
+++
@@ -1,8 +0,0 @@
-const sum = (a, b) => a + b;
-const sub = (a, b) => a - b;
-const mult = (a, b) => a * b;
-const div = (a, b) => ((b === 0) ? 'Do not divide by zero!' : a / b);
-
-export {
-  sum, sub, mult, div,
-};
