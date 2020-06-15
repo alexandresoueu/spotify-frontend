7218878615bcd6b27315557ee621c6f30ecb4e08
calexandredev <calexandredev@gmail.com>
2020-06-15 19:47:50 -0300

    AlbumList test working

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -1,11 +1,12 @@
 export default function renderAlbums(data, element) {
-    const markup = `<div class="list-item">
-        <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
-        <div class="list-description">
-          <p class="list-title">${data[0].name}</p>
-          <p class="list-subtitle">${data[0].artists[0].name}</p>
-        </div>
-    </div>`
+    const markup = data.map(album =>`
+        <div class="list-item">
+            <img src="${album.images[2].url}" alt="${album.name}" class="list-image">
+            <div class="list-description">
+                <p class="list-title">${album.name}</p>
+                <p class="list-subtitle">${album.artists[0].name}</p>
+            </div>
+        </div>`).join('')
 
     element.innerHTML = markup
 }
