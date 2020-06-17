ae21ad39c6c726ac8fd3c96655912e0b222b7937
calexandredev <calexandredev@gmail.com>
2020-06-17 19:33:33 -0300

    create AlbumList

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'TOKEN',
+  token: 'BQCsJduCD_c9LNe7k0gLK5r1inMjNl9UDsLACwO5w0VBthcYAuYh9-aeuvOAKi5LJjY9n3ukiVv36F_iSz_HLP5xp4QhcWRezlEM4Qxc_XvJuZu2_478I7VnAl8bOKNxzX6NcPwg38BEbzaQUFjB_9RbES16ZMuJiXkjGw',
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('NEMO SWIMMING.....');
+import spotify from './Spotify'
+import renderAlbums from './AlbumList'
+
+const albums = spotify.search.albums('soundgarden')
+const albumList = document.getElementById('album-list')
+
+albums
+  .then(data => renderAlbums(data.albums.items, albumList))
