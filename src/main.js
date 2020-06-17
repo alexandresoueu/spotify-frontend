ba286fe16d9e6324b9189fc715c55d0feec2ddb7
calexandredev <calexandredev@gmail.com>
2020-06-17 19:35:22 -0300

    adjust lint

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import spotify from './Spotify'
-import renderAlbums from './AlbumList'
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
 
-const albums = spotify.search.albums('soundgarden')
-const albumList = document.getElementById('album-list')
+const albums = spotify.search.albums('soundgarden');
+const albumList = document.getElementById('album-list');
 
 albums
-  .then(data => renderAlbums(data.albums.items, albumList))
+  .then(data => renderAlbums(data.albums.items, albumList));
