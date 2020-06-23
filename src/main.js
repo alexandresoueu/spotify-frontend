b65d9e9baab6bc372021204dccf8e033bfb4301e
calexandredev <calexandredev@gmail.com>
2020-06-23 15:20:59 -0300

    create AlbumInfo

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
-import renderAlbumInfo from './AlbumInfo'
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('soundgarden');
 const albumList = document.getElementById('album-list');
 
-const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4')
+const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
 const albumListInfo = document.getElementById('album-info');
 
 albums
   .then(data => renderAlbums(data.albums.items, albumList));
 
 album
-  .then(data => renderAlbumInfo(data, albumListInfo))
+  .then(data => renderAlbumInfo(data, albumListInfo));
+  
